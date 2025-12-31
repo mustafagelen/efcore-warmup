05e44eea4c8c6f69b3c3766ed14af17f1d5b5131
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2025-12-31 18:31:19 +0300

    Add project files.

---
+++ EntityRules/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ETicaretDbContext: DbContext
+{
+    public  DbSet<Product> Products { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //Provider
+        //Connection String
+        //Layz Loading
+    }
+
+}
+
+public class  Product
+{
+    
+}
