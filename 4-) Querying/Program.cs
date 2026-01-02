ff46651e6647ff57b9d3f8f3d6a833c7f6d2b0c4
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2026-01-02 16:18:11 +0300

    .

--- 4-) Querying/Program.cs
+++ 4-) Querying/Program.cs
@@ -77,7 +77,7 @@ var productsSingleOrDefault = await dbContext.Products.SingleOrDefaultAsync(p =>
 
 //First vs FirstOrDefault
 var productsFirst = await dbContext.Products.FirstAsync(p => p.Id == 55);
-var productsFirstOrDefault= await dbContext.Products.FirstOrDefaultAsync(p => p.Id == 55);
+var productsFirstOrDefault = await dbContext.Products.FirstOrDefaultAsync(p => p.Id == 55);
 
 //Find
 //Primary key columnu kullanarak tekil veri getiren sorgularda kullanılır. Repository Design Pattern'de sıkça kullanılır.
@@ -86,6 +86,68 @@ var productFind = await dbContext.Products.FindAsync(1);
 //Last vs LastOrDefault
 //Gelen verilerin sonuncusunu getirir.
 var productsLast = await dbContext.Products.LastAsync(p => p.Id > 2);
+#endregion
+#region Diğer Sorgulama Fonksiyonları
+//Count oluşturulan sorgunun execute edilmesi sonucunda kaç adet veri döneceğini verir. Count int LongCount ise long tipinde sonuç döner.
+var productCount = await dbContext.Products.CountAsync();
+
+//Any oluşturulan sorgunun execute edilmesi sonucunda en az bir adet veri dönüyorsa true döner. Hiç veri dönmüyorsa false döner.
+var isAnyProduct = await dbContext.Products.AnyAsync(p => p.Id > 5);
+
+//Max oluşturulan sorgunun execute edilmesi sonucunda ilgili kolondaki en büyük değeri döner.
+var maxProductId = await dbContext.Products.MaxAsync(p => p.Id);
+
+//Distinct oluşturulan sorgunun execute edilmesi sonucunda dönen verilerdeki tekrar eden kayıtları tekilleştirir.
+var distinctProducts = await dbContext.Products
+    .Select(p => p.ProductName)
+    .Distinct().ToListAsync();
+
+//Sum oluşturulan sorgunun execute edilmesi sonucunda ilgili kolondaki değerlerin toplamını döner.
+var totalProductIds = await dbContext.Products.SumAsync(p => p.Id);
+
+//All oluşturulan sorgunun execute edilmesi sonucunda dönen tüm verilerin belirtilen koşulu sağlaması durumunda true döner. Aksi halde false döner.
+var areAllProductsAboveIdZero = await dbContext.Products.AllAsync(p => p.Id > 0);
+
+//Contains oluşturulan sorgunun execute edilmesi sonucunda belirtilen değerin ilgili kolonda bulunup bulunmadığını kontrol eder. Bulunuyorsa true, bulunmuyorsa false döner.
+var containsProductName = await dbContext.Products
+    .Where(p => p.ProductName.Contains("7")).ToListAsync();
+
+//StartsWith EndsWith
+var startsWithAProducts = await dbContext.Products
+    .Where(p => p.ProductName.StartsWith("A")).ToListAsync();
+var endsWithZProducts = await dbContext.Products
+    .Where(p => p.ProductName.EndsWith("Z")).ToListAsync();
+#endregion
+#region Sorgu sonucu dönüşüm fonksiyonları
+//ToDictionaryAsync  ToList ile aynı amaca hizmet etmektedir. Ancak ToList gelen sorgu neticesini entity türünde bir koleksiyona dönüştürmekteyken ToDictionary gelen sorgu neticesini Dictionary türünde bir koleksiyona dönüştürür.
+var productsToDictionary = await dbContext.Products.ToDictionaryAsync(p => p.Id, p => p.ProductName);
+
+//ToArrayAsync Oluşturulan sorguyu dizi olarak elde eder. ToList ile aynı amaca hizmet eder.
+var productsToArray = await dbContext.Products.ToArrayAsync();
+
+//Select fonksiyonu işlevsel olarak birden fazla davranışı söz konusudur.
+//1. İlgili sorgudan belirli kolonları çekmek için kullanılır.
+var productsNames = await dbContext.Products.Select(p => new Product
+{
+    Id = p.Id,
+    Description = p.Description
+}).ToListAsync();
+
+//2. Select fonksiyonu gelen veriler farklı türlerde karşılamamızı sağlar "T"
+var productsNamesGeneric = await dbContext.Products.Select(p => new 
+{
+    Id = p.Id,
+    Description = p.Description
+}).ToListAsync();
+
+//SelectMany ilgili sorgudan gelen verilerin içerisindeki koleksiyonel yapıdaki verileri düzleştirerek tek bir koleksiyon halinde elde etmemizi sağlar.
+var productsPieces = await dbContext.Products.Include(p => p.Pieces).SelectMany(p => p.Pieces, (p, k) => new
+{
+    ProductName = p.ProductName,
+    PieceName = k.PieceName
+}).ToListAsync();
+
+
 #endregion
 
 
