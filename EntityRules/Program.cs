ef0d8e7973921a56c141992b5a6fc92f768f2b41
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2026-01-02 14:53:28 +0300

    .

--- EntityRules/Program.cs
+++ EntityRules/Program.cs
@@ -1,18 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+Console.WriteLine("");
 
-public class ETicaretDbContext: DbContext
+ETicaretDbContext context = new();
+Product product = new Product() { ProductName = "Laptop", Price = 15000 };
+
+//Product tablosuna yeni bir kayıt ekliyoruz.
+await context.Products.AddAsync(product);
+
+//SaveChanges; inserti update ve delete sorgularını oluşturup bir transaction eşliğinde veritabanına gönderip execute eden fonksiyondur.
+await context.SaveChangesAsync();
+
+public class ETicaretDbContext : DbContext
 {
-    public  DbSet<Product> Products { get; set; }
+    public DbSet<Product> Products { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=ECommerceDb;User Id=sa;Password=Mg123456;TrustServerCertificate=True;");
         //Provider
         //Connection String
-        //Layz Loading
+        //Layz Loading 
+        //Caching
     }
 
 }
 
-public class  Product
+//Her Entity primary key'e sahip olmalıdır.
+public class Product
 {
-    
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
 }
