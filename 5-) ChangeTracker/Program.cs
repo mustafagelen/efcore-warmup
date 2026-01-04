4629598e1b149d980b57c77fee40454a88015292
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2026-01-04 21:55:30 +0300

    Change Tracker Operations

--- 5-) ChangeTracker/Program.cs
+++ 5-) ChangeTracker/Program.cs
@@ -2,6 +2,36 @@
 ETicaretDbContext dbContext = new();
 //Change Tracker. Context nesnesi üzerinden gelen tüm veriler otomatik olarak bir takip mekanizması tarafından izlenir. Değişiklikler veya işlemler takip edilerek bu işlemlerin fıtratına uygun sql sorguları generate edilir.
 
+//Change Tracker Propertysi takip edilen nesnelere erişebilmemizi sağlar. Nesnelerin durumlarını görebiliriz.
+var products = await dbContext.Products.ToListAsync();
+var datas = dbContext.ChangeTracker.Entries();
+
+#region DetectChanges
+//DetectChanges metodu context nesnesi tarafından takip edilen nesnelerdeki değişiklikleri algılar. Normalde bu metodun manuel olarak çağrılmasına gerek yoktur. SaveChanges metodu çağrıldığında otomatik olarak çağrılır.(SnapChant)
+var product = await dbContext.Products.FirstAsync();
+product.ProductName = "Updated Name";
+dbContext.ChangeTracker.DetectChanges();
+#endregion
+#region Entries Methodu
+//Entries metodu context nesnesi tarafından takip edilen tüm nesnelere erişmemizi sağlar.
+var entries = dbContext.ChangeTracker.Entries();
+foreach (var entry in entries)
+{
+    Console.WriteLine($"Entity: {entry.Entity.GetType().Name}, State: {entry.State}");
+}
+#endregion
+#region AcceptAllChanges
+//AcceptAllChanges metodu context nesnesi tarafından takip edilen tüm nesnelerin durumlarını Unchanged olarak işaretler. Normalde bu metodun manuel olarak çağrılmasına gerek yoktur. SaveChanges metodu çağrıldığında otomatik olarak çağrılır.
+var newProduct = new Product { ProductName = "New Product", Description = "New Description" };
+dbContext.Products.Add(newProduct);
+dbContext.SaveChanges(); // Burada AcceptAllChanges otomatik olarak çağrılır.
+#endregion
+#region Interceptor Kullanımı
+dbContext.SaveChanges();
+
+#endregion
+
+
 Console.WriteLine("Done");
 public class ETicaretDbContext : DbContext
 {
@@ -14,6 +44,27 @@ public class ETicaretDbContext : DbContext
         //Provider
         //Connection String
         //Layz Loading
+    }
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken=default)
+    {
+        var entries = ChangeTracker.Entries();
+        foreach (var entry in entries)
+        {
+            if(entry.State == EntityState.Added)
+            {
+                Console.WriteLine($"Eklenecek Entity: {entry.Entity.GetType().Name}");
+            }
+            else if(entry.State == EntityState.Modified)
+            {
+                Console.WriteLine($"Güncellenecek Entity: {entry.Entity.GetType().Name}");
+            }
+            else if(entry.State == EntityState.Deleted)
+            {
+                Console.WriteLine($"Silinecek Entity: {entry.Entity.GetType().Name}");
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 
 }
