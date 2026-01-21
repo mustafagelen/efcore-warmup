000cb1f60d543e7ec49ec206b0c0e6097854cb07
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2026-01-21 12:53:33 +0300

    .

---
+++ 21-) LazyLoading/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+using System.Reflection.Emit;
+ETicaretDbContext dbContext = new();
+
+#region Lazy Loading
+//Navigation propertyler üzerinde bir işlem yapılmaya çalışıldığında o an ihtiyaç duyulan ilişkisel verilerin yüklenmesini sağlayan bir yöntemdir.
+
+#endregion
+#region Proxylerle Lazy Loading
+
+#endregion
+#region Proxy Olmaksızın Lazy Loading
+
+#endregion
+#region N+1 Problemi
+
+#endregion
+
+
+Console.WriteLine("Done");
+public class ETicaretDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Region> Regions { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+        optionsBuilder.UseSqlServer("Server=localhost;Database=EagerLoading;User Id=sa;Password=Mg123456;TrustServerCertificate=True;");
+        //Provider
+        //Connection String
+        //Layz Loading
+
+    }
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        //AutoInclude
+        modelBuilder.Entity<Employee>().Navigation(e => e.Region).AutoInclude();
+
+    }
+
+
+}
+
+
+public class Person
+{
+    public int Id { get; set; }
+
+}
+public class Employee : Person
+{
+    //public int Id { get; set; }
+    public int RegionId { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Salary { get; set; }
+
+    public List<Order> Orders { get; set; }
+    public Region Region { get; set; }
+}
+public class Region
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Employee> Employees { get; set; }
+}
+public class Order
+{
+    public int Id { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime OrderDate { get; set; }
+
+    public Employee Employee { get; set; }
+}
